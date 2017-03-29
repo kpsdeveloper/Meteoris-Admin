14b719202c6c12384b38d73cc2381a4a2d8a46d7
Kisphly Sup <kisphly.sup@gmail.com>
2017-03-29 18:14:20 +0400

    last

--- packages/meteoris:user/server/UserServer.js
+++ packages/meteoris:user/server/UserServer.js
@@ -119,23 +119,7 @@ Meteor.methods({
             MongoClient.connect( mongourl , function(err, db) {
                 new sql.Request().query("select TOP 1 * from CustomerTransacionDetailView AS CTD WHERE CTD.Barcode IN ("+barcode+") AND CTD.MemberID ="+memberId).then(function(recordset) {
                     if(recordset.length > 0){
-                        var docs = products.findOne({_id:data.productId});
-                        if(docs){
-                            var reviews = da.review;
-                            var dataReview = [];
-                            reviews.forEach( function(daa){
-                                if(data.userId == daa.user){
-                                    daa.verifypurchase = true;
-                                    dataReview.push(daa)   
-                                }else{
-                                    dataReview.push(daa)  
-                                }
-                            })
-                            products.update({_id:data.productId},{$set:{review:dataReview}});
-                            //db.collection('tmp_products').update({_id:data.productId},{$set:{review:dataReview}}); 
-                        }
-        
-
+                        Meteor.call('UpdateVerifyPurchase', data)
                     }
                 }).catch(function(err) {
                     console.log(err)
@@ -143,6 +127,23 @@ Meteor.methods({
             })
 
         })
+    },
+    UpdateVerifyPurchase: function(data){
+        var docs = products.findOne({_id:data.productId});
+        if(docs){
+            var reviews = docs.review;
+            var dataReview = [];
+            reviews.forEach( function(daa){
+                if(data.userId == daa.user){
+                    daa.verifypurchase = true;
+                    dataReview.push(daa)   
+                }else{
+                    dataReview.push(daa)  
+                }
+            })
+            products.update({_id:data.productId},{$set:{review:dataReview}});
+            //db.collection('tmp_products').update({_id:data.productId},{$set:{review:dataReview}}); 
+        }
     }
 });
 
